fc27bf788819ca7e343f699a37f923d0e6e4581d
이중현 <74997089+pfc01@users.noreply.github.com>
2022-10-18 20:52:50 +0900

    LCD 출력 기능 추가

--- arduinoCodes/soilMoisture/soilMoisture.ino
+++ arduinoCodes/soilMoisture/soilMoisture.ino
@@ -1,5 +1,12 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
   
 }
 
@@ -10,4 +17,7 @@ void loop() {
   Serial.print("\n");
   delay(1000);
 
+  lcd.setCursor(0,0);
+  lcd.print("Moisture : ");
+  lcd.print(soilMoisture);
 }
