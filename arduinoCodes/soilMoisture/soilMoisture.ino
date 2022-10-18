e43d9dd4fd0610dbcf6f6a78a6c2b2b111bb3ddf
이중현 <74997089+pfc01@users.noreply.github.com>
2022-10-18 22:42:37 +0900

    토양습도센서 코드와 블루투스 코드 결합

--- arduinoCodes/soilMoisture/soilMoisture.ino
+++ arduinoCodes/soilMoisture/soilMoisture.ino
@@ -1,23 +1,44 @@
-#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include "BluetoothSerial.h" //블루투스 시리얼 라이브러리 사용
+BluetoothSerial SerialBT;
 
 void setup() {
-  Serial.begin(9600);
-  lcd.init();
-  lcd.backlight();
-  
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  SerialBT.begin("ESP32"); // 표시될 블루투스 이름
+
 }
 
 void loop() {
+
+  
+  // put your main code here, to run repeatedly:  
   int soilMoisture = analogRead(A0);
-  Serial.print("Soil Moisture: ");
-  Serial.print(soilMoisture);
-  Serial.print("\n");
+
+
+  Serial.print("Soil Moisture : "); // 시리얼 모니터에 'OOO' 표시
+        Serial.println(soilMoisture);
   delay(1000);
 
-  lcd.setCursor(0,0);
-  lcd.print("Moisture : ");
-  lcd.print(soilMoisture);
+
+  if (Serial.available()) { // 블루투스 값을 읽어들이기
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) { //블루투스 값을 읽어들일 때
+    char text = SerialBT.read();
+    Serial.write(text); // 받은 블루투스 값을 그대로 시리얼 모니터에 출력
+    if(text == 'o'){ //만약 받은 신호가 'o'라면
+        Serial.print("Soil Moisture : "); // 시리얼 모니터에 'OOO' 표시
+        Serial.println(soilMoisture);
+        SerialBT.print("Soil Moisture : ");
+        SerialBT.println(soilMoisture);
+    } else if(text == 'x'){ //만약 받은 신호가 'x'라면
+        Serial.println("XXX"); //시리얼 모니터에 'XXX' 표시
+    }
+  }
+  
+  
+ Serial.print("Soil Moisture : ");
+  Serial.print(soilMoisture);
+ Serial.print("\n");  
+ delay(1000);
 }
