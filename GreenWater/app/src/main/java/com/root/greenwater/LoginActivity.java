0a5e374b9dc5d4ed5fdf296fc8e21421cf46d8fc
JH_T0mmy <wogur091511@naver.com>
2022-10-20 13:36:13 +0900

    임시로 로그인 버튼 클릭시 MainActivity 로 이동하게 프론트엔드 구현

--- GreenWater/app/src/main/java/com/root/greenwater/LoginActivity.java
+++ GreenWater/app/src/main/java/com/root/greenwater/LoginActivity.java
@@ -22,5 +22,14 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
