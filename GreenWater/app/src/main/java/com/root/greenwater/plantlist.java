58e9e77e076e5de3c6439c6ec733e50822094276
JH_T0mmy <wogur091511@naver.com>
2022-10-21 18:26:00 +0900

    식물 목록 화면 임시 리스트뷰 배치

--- GreenWater/app/src/main/java/com/root/greenwater/plantlist.java
+++ GreenWater/app/src/main/java/com/root/greenwater/plantlist.java
@@ -4,21 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class plantlist extends Fragment {
 
     private View view;
+    private ListView plantlist;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.plantlist, container, false);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // 다크모드 강제 비활성화
+
+        plantlist = view.findViewById(R.id.plantlist);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, data);
+        plantlist.setAdapter(adapter);
+
+        data.add("몬스테라");
+        data.add("페퍼민트");
+        data.add("산세베리아");
+        adapter.notifyDataSetChanged();
 
         return view;
     }
