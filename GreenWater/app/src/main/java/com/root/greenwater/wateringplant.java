bb316c7e1965b8328a5dfd2095efcb0689d2c6da
JH_T0mmy <wogur091511@naver.com>
2022-10-21 09:15:16 +0900

    화분 수분상태 모니터링탭 부분 임시 적용 Activity 동작 추가

--- GreenWater/app/src/main/java/com/root/greenwater/wateringplant.java
+++ GreenWater/app/src/main/java/com/root/greenwater/wateringplant.java
@@ -1,9 +1,14 @@
 package com.root.greenwater;
 
+import static java.lang.Integer.parseInt;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,12 +18,39 @@ import androidx.fragment.app.Fragment;
 public class wateringplant extends Fragment {
 
     private View view;
+    Button btn_adjust;
+    EditText et_humid;
+    TextView tv_humid;
+    TextView tv_status;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.wateringplant, container, false);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // 다크모드 강제 비활성화
+
+        btn_adjust = view.findViewById(R.id.btn_adjust);
+        et_humid = view.findViewById(R.id.et_humid);
+        tv_humid = view.findViewById(R.id.tv_humid);
+        tv_status = view.findViewById(R.id.tv_status);
+
+        btn_adjust.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_humid.setText((et_humid.getText()));
+
+            }
+        });
+
+        if (Integer.parseInt(tv_humid.getText().toString()) < 40) {
+            tv_status.setText("화분의 습도가 낮습니다.\n물을 주세요.");
+        }
+        else if (Integer.parseInt(tv_humid.getText().toString()) > 90) {
+            tv_status.setText("화분의 습도가 너무 높습니다.\n물을 줄이세요.");
+        }
+        else {
+            tv_status.setText("화분의 습도가 적당합니다.");
+        }
 
         return view;
     }
