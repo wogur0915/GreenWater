365f2e328306f1430b07b5acd06818dde0ad4f64
JH_T0mmy <wogur0915@gmail.com>
2022-11-27 17:01:07 +0900

    블루투스 on 기능 구현

--- GreenWater/app/src/main/java/com/root/greenwater/settings.java
+++ GreenWater/app/src/main/java/com/root/greenwater/settings.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -37,7 +38,7 @@ public class settings extends Fragment {
     private ArrayAdapter<String> mBTArrayAdapter;
 
     private Handler mHandler; // Our main handler that will receive callback notifications
-    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
+//    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
     private BluetoothSocket mBTSocket = null; // bi-directional client-to-client data path
 
     private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
@@ -54,7 +55,52 @@ public class settings extends Fragment {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // 다크모드 강제 비활성화
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mBtnBluetoothOn = view.findViewById(R.id.btnBluetooth_on);
+        mBtnBluetoothOff = view.findViewById(R.id.btnBluetooth_off);
+        mDiscoverBtn = view.findViewById(R.id.btn_discover);
+        mListPairedDevicesBtn = view.findViewById(R.id.btn_pairedList);
 
+        mBTArrayAdapter = new ArrayAdapter<String>(requireContext(),android.R.layout.simple_list_item_1);
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter(); // get a handle on the bluetooth radio
+
+        mDevicesListView = view.findViewById(R.id.devicesListView);
+        mDevicesListView.setAdapter(mBTArrayAdapter); // assign model to view
+//        mDevicesListView.setOnItemClickListener(mDeviceClickListener);
+
+        if (mBTArrayAdapter == null) {
+            Toast.makeText(requireContext(),"Bluetooth device not found!",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            mBtnBluetoothOn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    bluetoothOn(v);
+                }
+            });
+
+//            mBtnBluetoothOff.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v){
+//                    bluetoothOff(v);
+//                }
+//            });
+//
+//            mListPairedDevicesBtn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v){
+//                    listPairedDevices(v);
+//                }
+//            });
+//
+//            mDiscoverBtn.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v){
+//                    discover(v);
+//                }
+//            });
+        }
+
+        // 로그아웃 버튼 액션
         Button btn_logout = view.findViewById(R.id.btn_logout);
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,4 +115,19 @@ public class settings extends Fragment {
 
         return view;
     }
+
+    private void bluetoothOn(View view){
+        if (!mBTAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            Toast.makeText(requireContext(),"Bluetooth turned on",Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            Toast.makeText(requireContext(),"Bluetooth is already on", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 }
