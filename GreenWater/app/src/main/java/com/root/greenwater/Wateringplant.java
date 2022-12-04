082460a83074450bdc0adbce3852c34278a11888
JH_T0mmy <wogur0915@gmail.com>
2022-12-05 00:27:41 +0900

    수분 상태에 따른 화분 상태 이미지 뷰 적용

--- GreenWater/app/src/main/java/com/root/greenwater/Wateringplant.java
+++ GreenWater/app/src/main/java/com/root/greenwater/Wateringplant.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.core.app.NotificationCompat;
@@ -21,6 +22,7 @@ public class Wateringplant extends Fragment {
     private static TextView mtv_humid;
     private static TextView tv_status;
     private static NotificationHelper mNotificationhelper;
+    private static ImageView iv_plant;
     public static int count = 0;
 
     @Nullable
@@ -33,7 +35,7 @@ public class Wateringplant extends Fragment {
         tv_humid = view.findViewById(R.id.tv_humid);
         tv_status = view.findViewById(R.id.tv_status);
         mtv_humid = view.findViewById(R.id.tv_humid);
-
+        iv_plant = view.findViewById(R.id.plant_image);
         mNotificationhelper = new NotificationHelper(requireActivity());
 
         return view;
@@ -43,25 +45,29 @@ public class Wateringplant extends Fragment {
     {
         String cuttingMessage = message.replaceAll("[^\\d]", "");
         String calcHumid = calculateHumid(cuttingMessage);
-        mtv_humid.setText(calcHumid);
-        if (Integer.parseInt(mtv_humid.getText().toString()) >= 0 && Integer.parseInt(mtv_humid.getText().toString()) < 10) {
+        mtv_humid.setText(calcHumid + "%");
+        if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) >= 0
+                && Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) < 10) {
             tv_status.setText("화분의 습도가 낮습니다.\n물을 주세요.");
+            iv_plant.setImageResource(R.drawable.crying_plant);
             alertDelay();
         }
-        else if (Integer.parseInt(mtv_humid.getText().toString()) > 30) {
+        else if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) > 30) {
             count = 0;
             tv_status.setText("화분의 습도가 너무 높습니다.\n물을 줄이세요.");
+            iv_plant.setImageResource(R.drawable.toomuch_water);
         }
         else {
             count = 0;
             tv_status.setText("화분의 습도가 적당합니다.");
+            iv_plant.setImageResource(R.drawable.smile_plant);
         }
     }
 
     public static void alertDelay()
     {
         count = count + 1;
-        if (count == 1 || count % 750 == 0) {
+        if (count == 1 || count % 800 == 0) {
             sendOnChannel1("화분 습도 경고 알림", "화분의 습도가 낮습니다.\n물을 주세요.");
         }
 
