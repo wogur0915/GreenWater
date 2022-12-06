568502c6335e15a5b08b469185898c99bfde9968
JH_T0mmy <wogur0915@gmail.com>
2022-12-06 22:08:59 +0900

    식물 메모, 일지 메뉴에 작성할 게시글 틀 구성중

--- GreenWater/app/src/main/java/com/root/greenwater/WritePostActivity.java
+++ GreenWater/app/src/main/java/com/root/greenwater/WritePostActivity.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,31 +13,21 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Plantlist extends Fragment {
-
+public class WritePostActivity extends Fragment {
     private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        view = inflater.inflate(R.layout.plantlist, container, false);
+        view = inflater.inflate(R.layout.activity_write_post, container, false);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // 다크모드 강제 비활성화
 
         Button addlist_btn = view.findViewById(R.id.addlist_btn);
         addlist_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 로그아웃 하기
-                Intent intent = new Intent(requireActivity(), LoginActivity.class);
-                Toast.makeText(requireActivity(),"로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
-                requireActivity().finish();
-                startActivity(intent);
+
             }
         });
 
