31c40d1d9bb56007c43cf97685883c1db3eaf95f
JH_T0mmy <wogur0915@gmail.com>
2022-12-09 20:40:44 +0900

    식물 종류에 따른 라디오버튼 기능 추가

--- GreenWater/app/src/main/java/com/root/greenwater/Wateringplant.java
+++ GreenWater/app/src/main/java/com/root/greenwater/Wateringplant.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,9 +23,12 @@ public class Wateringplant extends Fragment {
     private View view;
     private static TextView mtv_humid;
     private static TextView tv_status;
+    private RadioGroup rg_plantgroup;
+    private RadioButton rdo_dryplant, rdo_wetplant, rdo_normalplant;
     private static NotificationHelper mNotificationhelper;
     private static ImageView iv_plant;
     public static int count = 0;
+    public static int plantFlag = 0;
 
     @Nullable
     @Override
@@ -35,13 +40,39 @@ public class Wateringplant extends Fragment {
         tv_status = view.findViewById(R.id.tv_status);
         mtv_humid = view.findViewById(R.id.tv_humid);
         iv_plant = view.findViewById(R.id.plant_image);
+        rg_plantgroup = view.findViewById(R.id.rg_plantgroup);
+        rdo_dryplant = view.findViewById(R.id.rdo_dryplant);
+        rdo_wetplant = view.findViewById(R.id.rdo_wetplant);
+        rdo_normalplant = view.findViewById(R.id.rdo_normalplant);
+
+
         mNotificationhelper = new NotificationHelper(requireActivity());
-        System.out.println(mtv_humid.getText().toString());
+
         if (mtv_humid.getText().toString() == "-1%") {
             tv_status.setText("모니터링 장치가 연결되지 않았습니다.");
             mtv_humid.setText("");
             iv_plant.setImageResource(R.drawable.connection_error);
         }
+
+        rg_plantgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rdo_normalplant:
+                        Toast.makeText(getActivity(), "보통 식물을 선택하셨습니다.", Toast.LENGTH_SHORT).show();
+                        plantFlag = 0;
+                        break;
+                    case R.id.rdo_dryplant:
+                        Toast.makeText(getActivity(), "건조한 식물을 선택하셨습니다.", Toast.LENGTH_SHORT).show();
+                        plantFlag = 1;
+                        break;
+                    case R.id.rdo_wetplant:
+                        Toast.makeText(getActivity(), "습식 식물을 선택하셨습니다.", Toast.LENGTH_SHORT).show();
+                        plantFlag = 2;
+                        break;
+                }
+            }
+        });
 
         return view;
     }
@@ -51,21 +82,63 @@ public class Wateringplant extends Fragment {
         String cuttingMessage = message.replaceAll("[^\\d]", "");
         String calcHumid = calculateHumid(cuttingMessage);
         mtv_humid.setText(calcHumid + "%");
-        if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) >= 0
-                && Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) < 10) {
-            tv_status.setText("화분의 습도가 낮습니다.\n물을 주세요.");
-            iv_plant.setImageResource(R.drawable.crying_plant);
-            alertDelay();
+        // 일반 식물
+        if (plantFlag == 0) {
+            if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) >= 0
+                    && Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) < 10) {
+                tv_status.setText("화분의 습도가 낮습니다.\n물을 주세요.");
+                iv_plant.setImageResource(R.drawable.crying_plant);
+                alertDelay();
+            }
+            else if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) > 30) {
+                count = 0;
+                tv_status.setText("화분의 습도가 너무 높습니다.\n물을 줄이세요.");
+                iv_plant.setImageResource(R.drawable.toomuch_water);
+            }
+            else {
+                count = 0;
+                tv_status.setText("화분의 습도가 적당합니다.");
+                iv_plant.setImageResource(R.drawable.smile_plant);
+            }
         }
-        else if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) > 30) {
-            count = 0;
-            tv_status.setText("화분의 습도가 너무 높습니다.\n물을 줄이세요.");
-            iv_plant.setImageResource(R.drawable.toomuch_water);
+        // 건조 식물
+        else if (plantFlag == 1) {
+            if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) >= 0
+                    && Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) < 5) {
+                tv_status.setText("화분의 습도가 낮습니다.\n물을 주세요.");
+                iv_plant.setImageResource(R.drawable.crying_plant);
+                alertDelay();
+            }
+            else if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) > 25) {
+                count = 0;
+                tv_status.setText("화분의 습도가 너무 높습니다.\n물을 줄이세요.");
+                iv_plant.setImageResource(R.drawable.toomuch_water);
+            }
+            else {
+                count = 0;
+                tv_status.setText("화분의 습도가 적당합니다.");
+                iv_plant.setImageResource(R.drawable.smile_plant);
+            }
         }
-        else {
-            count = 0;
-            tv_status.setText("화분의 습도가 적당합니다.");
-            iv_plant.setImageResource(R.drawable.smile_plant);
+
+        // 습식 식물
+        else if (plantFlag == 2) {
+            if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) >= 0
+                    && Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) < 15) {
+                tv_status.setText("화분의 습도가 낮습니다.\n물을 주세요.");
+                iv_plant.setImageResource(R.drawable.crying_plant);
+                alertDelay();
+            }
+            else if (Integer.parseInt(mtv_humid.getText().toString().replaceAll("[^\\d]", "")) > 35) {
+                count = 0;
+                tv_status.setText("화분의 습도가 너무 높습니다.\n물을 줄이세요.");
+                iv_plant.setImageResource(R.drawable.toomuch_water);
+            }
+            else {
+                count = 0;
+                tv_status.setText("화분의 습도가 적당합니다.");
+                iv_plant.setImageResource(R.drawable.smile_plant);
+            }
         }
     }
 
